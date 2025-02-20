e7be62070c841ef68e61a039bca4e927d3be710d
Cátia Nascimento <catianascimento.artist@gmail.com>
2025-02-20 17:33:18 +0000

    Add convert numbers

--- AskForInput/Program.cs
+++ AskForInput/Program.cs
@@ -6,8 +6,12 @@ namespace AskForInput
     {
         private static void Main(string[] args)
         {
-            string int = Console.ReadLine("Please write a number");
-            string int = Console.ReadLine("Please write a real number");
+            Console.WriteLine("Please write a number:");
+            string inteiro = Console.ReadLine();
+            Console.WriteLine("Please write a real number now:");
+            string real = Console.ReadLine();
+            converte_number = int.Parse( inteiro );
+            converte_realnumber = int.Parse( real );
 
         }
     }
