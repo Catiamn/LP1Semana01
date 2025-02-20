aaccb066e5c886a1aa24c4cf50b747ba58f66967
Cátia Nascimento <catianascimento.artist@gmail.com>
2025-02-20 17:21:49 +0000

    Add the command to ask for numbers

---
+++ AskForInput/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AskForInput
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            string int = Console.ReadLine("Please write a number");
+            string int = Console.ReadLine("Please write a real number");
+
+        }
+    }
+}
