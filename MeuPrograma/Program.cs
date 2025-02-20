5475af90cb04e42afb7535ff732d91223ab4183e
Cátia Nascimento <catianascimento.artist@gmail.com>
2025-02-20 15:00:51 +0000

    Add summaries

--- MeuPrograma/Program.cs
+++ MeuPrograma/Program.cs
@@ -2,8 +2,15 @@
 
 namespace MeuPrograma
 {
+    /// <summary>
+    /// Creation of the class Program where we run the code 
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// Empty string that will display the text written
+        /// </summary>
+        /// <param name="args"></param>
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello LP1! \nBye World");
