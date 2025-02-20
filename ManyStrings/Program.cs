4652dfd83a75168eb9f159177a4926d4c1552c58
Cátia Nascimento <catianascimento.artist@gmail.com>
2025-02-20 16:53:03 +0000

    Add strings

---
+++ ManyStrings/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ManyStrings
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+           string welcome = "Welcome to LP1!";
+           string example = "Um tab\t e um copyright\u03A9";
+           string symbol = "This is a heart ❤";
+           Console.WriteLine($"{welcome} \n{example} \n{symbol}");
+        }
+    }
+}
