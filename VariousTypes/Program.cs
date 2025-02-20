c22eb68f40a3e37b035380824ce2476431c8d87e
Cátia Nascimento <catianascimento.artist@gmail.com>
2025-02-20 15:57:59 +0000

    Add int numbers

---
+++ VariousTypes/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace VariousTypes
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            int number = 12;
+            int another = 9;
+            int last = 5;
+            Console.WriteLine($"{number} \n{another} \n{last}");
+        }
+    }
+}
