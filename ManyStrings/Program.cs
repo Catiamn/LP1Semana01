c47470d32d82bf2f90ce0e786723cc38bacc20ae
Cátia Nascimento <catianascimento.artist@gmail.com>
2025-02-20 17:13:41 +0000

    Add string formations

--- ManyStrings/Program.cs
+++ ManyStrings/Program.cs
@@ -6,17 +6,23 @@ namespace ManyStrings
     {
         private static void Main(string[] args)
         {
+           double xx = 1.23456;
+           int ii = 19;
+           string xx1 = $"xx={xx:f2}";
+           string xx2 = $"xx={xx:p1}";
+           string ii1 = $"ii={ii:X}";
+           string ii2 = $"ii={ii:C}";
            string welcome = "Welcome to LP1!";
            string example = "Um tab\t e um copyright\u03A9";
            string symbol = "This is a heart ❤";
            Console.WriteLine($"{welcome} \n{example} \n{symbol}");
            int x = 4;
            int y = 6;
-           string a_plus_x = "a" + 2;
+           string a_plus_x = "a" + 2; 
            string abc_plus_x = "abc" + x;
            string x_plus_y = $"{x} mais {y} e’ igual a {x + y}";
            Console.WriteLine($"{a_plus_x} \n{abc_plus_x} \n{x_plus_y}");
-
+           Console.WriteLine($"{xx1} \n{xx2} \n{ii1} \n{ii2}");
         }
     }
 }
