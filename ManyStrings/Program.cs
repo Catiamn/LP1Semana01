e533993f06b4426ee56bc83bff8f5a934a2e58c8
Cátia Nascimento <catianascimento.artist@gmail.com>
2025-02-20 17:01:06 +0000

    Add more strings

--- ManyStrings/Program.cs
+++ ManyStrings/Program.cs
@@ -10,6 +10,13 @@ namespace ManyStrings
            string example = "Um tab\t e um copyright\u03A9";
            string symbol = "This is a heart ❤";
            Console.WriteLine($"{welcome} \n{example} \n{symbol}");
+           int x = 4;
+           int y = 6;
+           string a_plus_x = "a" + 2;
+           string abc_plus_x = "abc" + x;
+           string x_plus_y = $"{x} mais {y} e’ igual a {x + y}";
+           Console.WriteLine($"{a_plus_x} \n{abc_plus_x} \n{x_plus_y}");
+
         }
     }
 }
