47dcb01b232cea597d9849689e8315b4c0f4f3c8
Cátia Nascimento <catianascimento.artist@gmail.com>
2025-02-21 13:28:57 +0000

    Added text to help the user run the code smootly

--- AskForInput/Program.cs
+++ AskForInput/Program.cs
@@ -7,11 +7,11 @@ namespace AskForInput
         private static void Main(string[] args)
         {
             CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
-            Console.WriteLine("Please write a number:");
+            Console.WriteLine("Please write a number (Without decimals):");
             string inteiro = Console.ReadLine();
             int converte_number = int.Parse( inteiro );
             
-            Console.WriteLine("Please write another number now:");
+            Console.WriteLine("Please write another number now (Remember to only use dots when referring to the decimal):");
             string real = Console.ReadLine();
             float converte_realnumber = float.Parse( real );
             float result = converte_number + converte_realnumber;
