c048b5117102f82e1026292a3f7876a2fd0e5c45
Cátia Nascimento <catianascimento.artist@gmail.com>
2025-02-21 14:33:41 +0000

    Added Commentary

--- Percentagem/Program.cs
+++ Percentagem/Program.cs
@@ -7,12 +7,16 @@ namespace Percentagem
     {
         private static void Main(string[] args)
         {
-            CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;            
+            ///Small Script to make sure only dots show up and not commas
+            CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;  
+            ///Ask use for the 3 codes that are gonna be used          
             Console.WriteLine("Please write 4 numbers between 0 and 1 (Remember to only use dots when referring to the decimal):");
-            string one = Console.ReadLine();
+            ///Change number given to float type and to percentage followed by two decimals
+            string one = Console.ReadLine(); 
             float realone = float.Parse( one );
             string numberone = $"{realone:p2}";
             Console.WriteLine();
+            ///Repeat the process of the first number to the other three numbers necessary
             string two = Console.ReadLine();
             float realtwo = float.Parse( two );
             string numbertwo = $"{realtwo:p2}";
@@ -24,6 +28,7 @@ namespace Percentagem
             string four = Console.ReadLine();
             float realfour = float.Parse( four );
             string numberfour = $"{realfour:p2}";
+            ///Give the user the 4 numbers at the start but in percentage and with two decimals
             Console.WriteLine($"{numberone} \n{numbertwo} \n{numberthree} \n{numberfour}");
         }
     }
