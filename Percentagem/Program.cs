4060ddf81149fd1e9d374702ae061a78f79587cd
Cátia Nascimento <catianascimento.artist@gmail.com>
2025-02-21 14:27:28 +0000

    Add percentages at the end

--- Percentagem/Program.cs
+++ Percentagem/Program.cs
@@ -11,16 +11,20 @@ namespace Percentagem
             Console.WriteLine("Please write a number between 0 and 1 (Remember to only use dots when referring to the decimal):");
             string one = Console.ReadLine();
             float realone = float.Parse( one );
+            string numberone = $"realone={realone:p2}";
             Console.WriteLine("Please write another number between 0 and 1 (Remember to only use dots when referring to the decimal):");
             string two = Console.ReadLine();
-            float realrtwo = float.Parse( two );
+            float realtwo = float.Parse( two );
+            string numbertwo = $"realtwo={realtwo:p2}";
             Console.WriteLine("Please write another number between 0 and 1 (Remember to only use dots when referring to the decimal):");
             string three = Console.ReadLine();
             float realthree = float.Parse( three );
+            string numberthree = $"realthree={realthree:p2}";
             Console.WriteLine("Please write another number between 0 and 1 (Remember to only use dots when referring to the decimal):");
             string four = Console.ReadLine();
             float realfour = float.Parse( four );
-            Console.WriteLine("Here's all the numbers you picked: \n{realone} \n{realtwo} \n{realthree} \n{realfour}");
+            string numberfour = $"realfour={realfour:p2}";
+            Console.WriteLine($"{numberone} \n{numbertwo} \n{numberthree} \n{numberfour}");
         }
     }
 }
