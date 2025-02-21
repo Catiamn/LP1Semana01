5f889f2182847ada1767ce69cdd8350c3287c8e9
Cátia Nascimento <catianascimento.artist@gmail.com>
2025-02-21 13:25:45 +0000

    Fixed some erros with the code

--- AskForInput/Program.cs
+++ AskForInput/Program.cs
@@ -1,18 +1,21 @@
 ﻿using System;
-
+using System.Globalization;
 namespace AskForInput
 {
     public class Program
     {
         private static void Main(string[] args)
         {
+            CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
             Console.WriteLine("Please write a number:");
             string inteiro = Console.ReadLine();
+            int converte_number = int.Parse( inteiro );
+            
             Console.WriteLine("Please write another number now:");
             string real = Console.ReadLine();
-            int converte_number = int.Parse( inteiro );
             float converte_realnumber = float.Parse( real );
             float result = converte_number + converte_realnumber;
+            
             Console.WriteLine($"Together they equal: {result}");
         }
     }
