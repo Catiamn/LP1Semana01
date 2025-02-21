8e54697d544f529709ceea99571904f1d18883d3
Cátia Nascimento <catianascimento.artist@gmail.com>
2025-02-21 14:29:24 +0000

    Reduce text

--- Percentagem/Program.cs
+++ Percentagem/Program.cs
@@ -8,22 +8,22 @@ namespace Percentagem
         private static void Main(string[] args)
         {
             CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;            
-            Console.WriteLine("Please write a number between 0 and 1 (Remember to only use dots when referring to the decimal):");
+            Console.WriteLine("Please write 4 numbers between 0 and 1 (Remember to only use dots when referring to the decimal):");
             string one = Console.ReadLine();
             float realone = float.Parse( one );
-            string numberone = $"realone={realone:p2}";
-            Console.WriteLine("Please write another number between 0 and 1 (Remember to only use dots when referring to the decimal):");
+            string numberone = $"{realone:p2}";
+            Console.WriteLine();
             string two = Console.ReadLine();
             float realtwo = float.Parse( two );
-            string numbertwo = $"realtwo={realtwo:p2}";
-            Console.WriteLine("Please write another number between 0 and 1 (Remember to only use dots when referring to the decimal):");
+            string numbertwo = $"{realtwo:p2}";
+            Console.WriteLine();
             string three = Console.ReadLine();
             float realthree = float.Parse( three );
-            string numberthree = $"realthree={realthree:p2}";
-            Console.WriteLine("Please write another number between 0 and 1 (Remember to only use dots when referring to the decimal):");
+            string numberthree = $"{realthree:p2}";
+            Console.WriteLine();
             string four = Console.ReadLine();
             float realfour = float.Parse( four );
-            string numberfour = $"realfour={realfour:p2}";
+            string numberfour = $"{realfour:p2}";
             Console.WriteLine($"{numberone} \n{numbertwo} \n{numberthree} \n{numberfour}");
         }
     }
